219e5b206d84172a84762a06c7f33ef957396543
sukruburakcetin <cetin.sukruburak@gmail.com>
2024-02-18 21:49:48 +0300

    unnecessary part has been removed since system cannot read memory allocation without being used

--- check_cuda.py
+++ check_cuda.py
@@ -7,6 +7,3 @@ print()
 # Additional Info when using cuda
 if device.type == 'cuda':
     print(torch.cuda.get_device_name(0))
-    print('Memory Usage:')
-    print('Allocated:', round(torch.cuda.memory_allocated(0)/1024**3,1), 'GB')
-    print('Cached:   ', round(torch.cuda.memory_cached(0)/1024**3,1), 'GB')
